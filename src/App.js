e46b826f2c55419b087b73779409628616811f69
MinhTung2504 <minhtungg2504@gmail.com>
2022-07-18 09:27:21 +0700

    Done project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// useReducer
 
+import { useReducer, useRef } from "react";
+import { addJob, deleteJob, setJob } from "./reducers/actions/actions";
+import { reducer } from "./reducers/reducer";
+
+const initState = {
+  job: "",
+  jobs: [],
+};
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState);
+
+  const { job, jobs } = state;
+
+  const inputRef = useRef();
+
+  const handleSubmit = () => {
+    dispatch(addJob(job));
+    dispatch(setJob(""));
+    inputRef.current.focus();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        padding: "0 20px",
+        textAlign: "center",
+      }}
+    >
+      <h1>To do App With Reducer</h1>
+      <input
+        ref={inputRef}
+        value={job}
+        placeholder="Enter todo..."
+        onChange={(e) => {
+          dispatch(setJob(e.target.value));
+        }}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>
+            {job}{" "}
+            <span
+              onClick={() => {
+                dispatch(deleteJob(index));
+              }}
+            >
+              &times;
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/reducers/actions/actions.js
@@ -0,0 +1,34 @@
+// 2. Actions
+const SET_JOB = "set_job";
+const ADD_JOB = "add_job";
+const DELETE_JOB = "delete_job";
+
+const setJob = (payload) => {
+  return {
+    type: SET_JOB,
+    payload,
+  };
+};
+
+const addJob = (payload) => {
+  return {
+    type: ADD_JOB,
+    payload,
+  };
+};
+
+const deleteJob = (payload) => {
+  return {
+    type: DELETE_JOB,
+    payload,
+  };
+};
+
+module.exports = {
+  SET_JOB,
+  ADD_JOB,
+  DELETE_JOB,
+  setJob,
+  addJob,
+  deleteJob,
+};
